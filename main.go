78f5d9e9e0849bbd7167669f6837a33dace70dac
Claire West <bokthaku@gmail.com>
2024-07-12 20:18:40 -0400

    Rewrite in Go

---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"sync"
+	"syscall"
+)
+
+type Service struct {
+	path string
+	port int
+}
+
+func main() {
+	var waitGroup sync.WaitGroup
+
+	services := readServices()
+	waitGroup.Add(len(services))
+
+	var servers []*http.Server
+	for _, service := range services {
+		server := startService(service)
+		server.RegisterOnShutdown(func() {
+			log.Println("No longer listening on port", service.port)
+			waitGroup.Done()
+		})
+		servers = append(servers, server)
+	}
+
+	done := make(chan os.Signal, 1)
+    signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+    <-done
+
+    ctx := context.Background()
+    for _, server := range servers {
+    	server.Shutdown(ctx)
+    }
+
+	waitGroup.Wait()
+}
+
+func readServices() []Service {
+	read, err := os.Open(os.Args[1])
+	if err != nil { panic(err) }
+
+	scanner := bufio.NewScanner(read)
+
+	var services []Service
+	for scanner.Scan() {
+		parts := strings.Split(scanner.Text(), ",")
+		port, err := strconv.Atoi(parts[1])
+		if err != nil { panic(err) }
+
+		services = append(services, Service{
+			path: parts[0],
+			port: port,
+		})
+	}
+
+	return services
+}
+
+func startService(service Service) *http.Server {
+    fsHandler := http.FileServer(http.Dir(service.path))
+    handler := http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+   		log.Println(req.URL.Path)
+   		fsHandler.ServeHTTP(resp, req)
+    })
+
+	server := &http.Server{
+		Addr: ":" + strconv.Itoa(service.port),
+		Handler: handler,
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+	log.Println("Serving", service.path, "on port", service.port)
+
+    return server
+}
