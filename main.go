6d894cfcdfca229195e336678e909b0fcb4aac09
Claire West <bokthaku@gmail.com>
2024-10-27 18:28:24 -0400

    Various improvements

--- main.go
+++ main.go
@@ -1,24 +1,22 @@
 /*
-dev-srv hosts multiple static file directories, each on its own port.
+dev-srv serves multiple static file directories, each on its own port.
 
 Usage:
 
-	dev-srv
-	dev-srv [<file>]
-
-Define a set of services in a file in the following format:
-
-	8080=/home/user/git/myfirstproject
-	9090=../coolwebthing/public
-
-The default value for <file> is "./services", relative to the location of the executable.
+	dev-srv [flags] [<file>]
+	dev-srv [?|help|usage]
 */
 package main
 
 import (
 	"bufio"
 	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"io/fs"
 	"log"
+	"math"
 	"net/http"
 	"os"
 	"os/signal"
@@ -27,60 +25,152 @@ import (
 	"strings"
 	"sync"
 	"syscall"
+	"time"
 )
 
 type Service struct {
+	dir  http.Dir
 	path string
 	port int
 }
 
-const Y = "\033[33m"
-const B = "\033[34m"
-const C = "\033[36m"
-const R = "\033[0m"
+const (
+	R = "\033[31m"
+	G = "\033[32m"
+	Y = "\033[33m"
+	B = "\033[34m"
+	M = "\033[35m"
+	C = "\033[36m"
+	W = "\033[37m"
+	X = "\033[0m"
+)
 
-func main() {
-	var serviceFile string
-	executable, err := os.Executable()
-	if err != nil { panic(err) }
-	dir := filepath.Dir(executable);
+type RespFlagValue int
 
-	if len(os.Args) < 2 || len(os.Args[1]) == 0 {
-		serviceFile = dir + "/services"
-	} else {
-		serviceFile = os.Args[1]
+var RespFlag RespFlagValue
+
+const (
+	Invalid RespFlagValue = iota
+	None
+	Status
+	Short
+	Long
+)
+
+func flags() {
+	respvals := "none|status|short|long"
+	respdef := "none"
+	respflagmap := map[string]RespFlagValue{
+		"":       Invalid,
+		"none":   None,
+		"status": Status,
+		"short":  Short,
+		"long":   Long,
 	}
 
-	var waitGroup sync.WaitGroup
+	resparg := flag.String("resp", respdef, fmt.Sprintf("Logging options for the response. [%s]", respvals))
 
-	services := readServices(serviceFile)
-	waitGroup.Add(len(services))
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `Usage:	%[1]s
+	%[1]s [flags] [<file>]
+	%[1]s [?|help|usage]
 
-	var servers []*http.Server
-	for _, service := range services {
-		server := startService(service, filepath.Dir(serviceFile))
-		server.RegisterOnShutdown(func() {
-			log.Printf("%s%d%s → %sStopped%s\n", Y, service.port, R, C, R)
-			waitGroup.Done()
-		})
-		servers = append(servers, server)
+Define a list of services in a file with the following format. The default value for <file> is "./services", relative to the location of %[1]s.
+
+	8080=/home/user/git/myfirstproject
+	9090=../coolwebthing/public
+
+Flags:
+`, filepath.Base(os.Args[0]))
+		flag.PrintDefaults()
 	}
 
-	interrupted := make(chan os.Signal, 1)
-    signal.Notify(interrupted, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-    <-interrupted
+	flag.Parse()
 
-    ctx := context.Background()
-    for _, server := range servers {
-    	server.Shutdown(ctx)
-    }
-
-	waitGroup.Wait()
+	RespFlag = respflagmap[*resparg]
+	if RespFlag == Invalid {
+		RespFlag = respflagmap[respdef]
+		fmt.Printf("\nInvalid value for flag -resp [%s]\n", respvals)
+		fmt.Printf("Continuing with default value \"%s\"\n\n", respdef)
+	}
 }
 
-func readServices(fileName string) []Service {
-	read, err := os.Open(fileName)
-	if err != nil { panic(err) }
+func main() {
+	flags()
+	firstarg := flag.Arg(0)
+	if strings.EqualFold(firstarg, "usage") || strings.EqualFold(firstarg, "help") || firstarg == "?" {
+		flag.Usage()
+		return
+	}
+
+	var servicefile string
+	if len(flag.Args()) == 0 || len(firstarg) == 0 {
+		executable, _ := os.Executable()
+		servicefile = filepath.Dir(executable) + "/services"
+	} else {
+		servicefile = firstarg
+	}
+
+	services := readServices(servicefile)
+	start(services)
+}
+
+func start(services []Service) {
+	var wg sync.WaitGroup
+	wg.Add(len(services))
+	wait := make(chan struct{})
+	go func() {
+		wg.Wait()
+		wait <- struct{}{}
+	}()
+
+	servers := make(map[int]*http.Server)
+	for _, service := range services {
+		server := startService(service)
+		servers[service.port] = server
+		server.RegisterOnShutdown(func() {
+			log.Printf("%s%d%s → %sStopped%s\n", Y, service.port, X, C, X)
+			delete(servers, service.port)
+			wg.Done()
+		})
+	}
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case <-wait:
+		os.Exit(1)
+	case <-interrupt:
+		fmt.Println()
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+	defer cancel()
+
+	for _, server := range servers {
+		go func() {
+			if err := stopService(server, ctx); err != nil {
+				wg.Done()
+			}
+		}()
+	}
+
+	select {
+	case <-wait:
+	case <-interrupt:
+		fmt.Println()
+	}
+}
+
+func readServices(servicefile string) []Service {
+	read, err := os.Open(servicefile)
+	if errors.Is(err, fs.ErrNotExist) {
+		log.Fatalf("%sUnable to read file%s: %s", R, X, servicefile)
+	}
+	if err != nil {
+		panic(err)
+	}
 
 	scanner := bufio.NewScanner(read)
 
@@ -88,9 +178,12 @@ func readServices(fileName string) []Service {
 	for scanner.Scan() {
 		parts := strings.Split(scanner.Text(), "=")
 		port, err := strconv.Atoi(parts[0])
-		if err != nil { panic(err) }
+		if err != nil {
+			log.Fatalf("%sInvalid port number%s: %s", R, X, parts[0])
+		}
 
 		services = append(services, Service{
+			dir:  http.Dir(filepath.Dir(servicefile) + "/" + parts[1]),
 			path: parts[1],
 			port: port,
 		})
@@ -104,7 +197,7 @@ type DirWithHtmlFallback struct {
 	dir http.Dir
 }
 
-func (d DirWithHtmlFallback) Open (name string) (http.File, error) {
+func (d DirWithHtmlFallback) Open(name string) (http.File, error) {
 	f, err := d.dir.Open(name)
 	if os.IsNotExist(err) && filepath.Ext(name) == "" {
 		f, err = d.dir.Open(name + ".html")
@@ -112,28 +205,92 @@ func (d DirWithHtmlFallback) Open (name string) (http.File, error) {
 	return f, err
 }
 
-func startService(service Service, rootDir string) *http.Server {
-	dir := DirWithHtmlFallback{http.Dir(rootDir + "/" + service.path)}
-    fsHandler := http.FileServer(dir)
-    handler := http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
-   		log.Printf("%s%s%s%s%s", C, service.path, B, req.URL.Path, R)
+type RespWriterWithStatus struct {
+	resp   http.ResponseWriter
+	status int
+	body   []byte
+}
 
-     	resp.Header().Add("access-control-allow-origin", "*")
-   		fsHandler.ServeHTTP(resp, req)
-    })
+func WrapResponseWriter(resp http.ResponseWriter) *RespWriterWithStatus {
+	return &RespWriterWithStatus{resp, 200, []byte{}}
+}
+
+func (resp *RespWriterWithStatus) Header() http.Header {
+	return resp.resp.Header()
+}
+
+func (resp *RespWriterWithStatus) Write(bytes []byte) (int, error) {
+	resp.body = bytes
+	return resp.resp.Write(bytes)
+}
+
+func (resp *RespWriterWithStatus) WriteHeader(code int) {
+	resp.status = code
+	resp.resp.WriteHeader(code)
+}
+
+func LoggingHandler(next http.Handler, service Service) http.Handler {
+	return http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+		wrapped := WrapResponseWriter(resp)
+
+		log.Printf("%s%d%s → %s%s %s%s%s%s%s", Y, service.port, X, G, req.Method, C, service.path, B, req.URL.Path, X)
+
+		next.ServeHTTP(wrapped, req)
+
+		if RespFlag == None {
+			return
+		}
+
+		if RespFlag == Status {
+			log.Printf("%s%d%s ← %s%d %s%s", Y, service.port, X, M, wrapped.status, http.StatusText(wrapped.status), X)
+			return
+		}
+
+		var body string
+		if len(wrapped.body) > 0 {
+			if RespFlag == Short {
+				bytes := wrapped.body[0:int(math.Min(float64(len(wrapped.body)), 50.0))]
+				body = strings.ReplaceAll(string(bytes), "\n", "")
+				if len(wrapped.body) > 50 {
+					body = body + "..."
+				}
+			} else if RespFlag == Long {
+				body = "\n" + string(wrapped.body)
+			}
+		}
+
+		log.Printf("%s%d%s ← %s%d %s %s%s%s", Y, service.port, X, M, wrapped.status, http.StatusText(wrapped.status), W, string(body), X)
+	})
+}
+
+func startService(service Service) *http.Server {
+	dir := DirWithHtmlFallback{service.dir}
+	handler := LoggingHandler(http.FileServer(dir), service)
 
 	server := &http.Server{
 		Addr: ":" + strconv.Itoa(service.port),
-		Handler: handler,
+		Handler: http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+			resp.Header().Add("access-control-allow-origin", "*")
+			handler.ServeHTTP(resp, req)
+		}),
 	}
 
 	go func() {
-		err := server.ListenAndServe()
-		if err != nil && err != http.ErrServerClosed {
-			log.Fatal(err)
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			log.Printf("%s%d%s → %sError%s %v", Y, service.port, X, R, X, err)
+			stopService(server, context.Background())
 		}
 	}()
-	log.Printf("%s%d%s → %s%s%s\n", Y, service.port, R, C, service.path, R)
 
-    return server
+	log.Printf("%s%d%s → %s%s%s\n", Y, service.port, X, C, service.path, X)
+	return server
+}
+
+func stopService(server *http.Server, ctx context.Context) error {
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("%sError shutting down server%s: %v", R, X, err)
+		server.Close()
+		return err
+	}
+	return nil
 }
